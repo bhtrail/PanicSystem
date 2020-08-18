57f102c5f8579a1728121b857d6b26153dcdafc9
Mhburg <Morgan_lee5@hotmail.com>
2020-08-18 16:12:38 -0400

    Remove pop-up dialog and refactor some reflection code

--- Logger.cs
+++ Logger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Harmony;
+using HBS.Logging;
 using static PanicSystem.PanicSystem;
 
 // ReSharper disable ClassNeverInstantiated.Global
@@ -9,7 +10,11 @@ namespace PanicSystem
 {
     public class Logger
     {
+        private const string LOGGER_NAME = "PanicSystem";
+
         private static string LogFilePath => Path.Combine(modDirectory, "log.txt");
+
+        private static ILog logger = HBS.Logging.Logger.GetLogger(LOGGER_NAME, LogLevel.Error);
 
         public static void LogReport(object line)
         {
@@ -40,6 +45,16 @@ namespace PanicSystem
             {
                 FileLog.Log($"[PanicSystem] {input ?? "null"}");
             }
+        }
+
+        public static void LogError(object message, Exception exception)
+        {
+            logger.LogError(message, exception);
+        }
+
+        public static void LogError(object message)
+        {
+            logger.LogError(message);
         }
     }
 }
